de7e08bc96560336e32a7f593970fcfb46e2a346
Lawrence Brown <lawrence.brown@gmail.com>
2013-05-10 11:28:29 -0500

    add starter gem and ran rails g starter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitTest::Application.config.secret_token = '5c0fe4189820deb466d3cfb5bc2ff6241c4217fd9781db4715e78a7bb3440140bd5b7242cda6d43f22a32b426d1b82d04f3b954adf6d55815fa455f0e21684d0'
